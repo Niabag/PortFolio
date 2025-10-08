28f64c1459a5dc60d4ce101db4b691ce6f963002
Niabag <gabain@hotmail.fr>
2025-10-08 17:09:53 +0200

    ii

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -127,7 +127,7 @@ export default function Navbar() {
       </div>
 
       <div
-        className={`fixed top-0 left-0 h-screen w-screen bg-black transform transition-transform duration-300 md:hidden ${open ? 'translate-x-0' : 'translate-x-full'}`}
+        className={`fixed top-0 right-0 h-screen w-screen bg-black transform transition-transform duration-300 md:hidden z-[2000] ${open ? 'translate-x-0' : 'translate-x-full'}`}
       >
         <div className="pt-20 px-6">
           <div className="mb-6">
