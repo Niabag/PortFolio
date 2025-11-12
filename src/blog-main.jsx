b71f0577140ad566e0f40b515404232df11d5fad
Niabag <gabain@hotmail.fr>
2025-11-12 21:45:28 +0100

    feat: add chat buttons to blog index page

--- src/blog-main.jsx
+++ src/blog-main.jsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { LanguageProvider } from './LanguageContext';
 import BlogPage from './pages/BlogPage';
+import WhatsAppButton from './components/WhatsAppButton';
+import TawkToChat from './components/TawkToChat';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <LanguageProvider>
+        <WhatsAppButton />
+        <TawkToChat />
         <BlogPage />
       </LanguageProvider>
     </BrowserRouter>
